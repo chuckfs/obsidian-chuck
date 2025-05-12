2ff2fa4a57dbacd99fb352d85c63f505c76b07c2
CHUCK <204468377+chuckfs@users.noreply.github.com>
2025-05-12 12:33:44 -0400

    Update custommodal.ts

--- custommodal.ts
+++ custommodal.ts
@@ -1,23 +1,41 @@
 import { App, Modal, TextComponent, ButtonComponent } from "obsidian";
+import { localHashEmbed, cosineSimilarity } from "./embedding";
+import { vectorStore, loadVectorStore } from "./semanticSearch";
 
 export class CustomModal extends Modal {
-  constructor(app: App) {
-    super(app);
-  }
-
-  onOpen() {
+  async onOpen() {
     const { contentEl } = this;
     contentEl.createEl("h2", { text: "Ask your vault:" });
 
     const input = new TextComponent(contentEl);
     input.inputEl.style.width = "100%";
 
+    const resultContainer = contentEl.createDiv();
+
+    await loadVectorStore(this.app);
+
     new ButtonComponent(contentEl)
       .setButtonText("Ask")
       .onClick(async () => {
         const query = input.getValue();
-        contentEl.createEl("p", { text: `Query: ${query}` });
-        // TODO: Add search + inference logic
+        const queryVec = localHashEmbed(query);
+
+        const results = Object.entries(vectorStore)
+          .map(([path, vec]) => ({
+            path,
+            score: cosineSimilarity(queryVec, vec),
+          }))
+          .sort((a, b) => b.score - a.score)
+          .slice(0, 5);
+
+        resultContainer.empty();
+        resultContainer.createEl("h4", { text: "Top Matches:" });
+
+        results.forEach((res, i) => {
+          resultContainer.createEl("div", {
+            text: `${i + 1}. ${res.path} (${res.score.toFixed(3)})`,
+          });
+        });
       });
   }
 
